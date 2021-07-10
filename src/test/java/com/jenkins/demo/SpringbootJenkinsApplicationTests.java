9570dd3220f40503474868af4f4cc5282aadf038
sreenugithub <csreenu.soft@gmail.com>
2021-07-10 23:56:10 +0530

    fisrt commit

---
+++ src/test/java/com/jenkins/demo/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jenkins.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+	static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Testcase executing ....");
+		assertEquals(true, true);
+	}
+
+	
+}
