3b44ef6bbadafb3ae7f6f92d1f05437c34d09aaa
sreenugithub <csreenu.soft@gmail.com>
2021-07-11 23:32:40 +0530

    path changes
     path changes

--- src/main/java/com/jenkins/demo/controller/TestController.java
+++ src/main/java/com/jenkins/demo/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	
-@GetMapping("/message")
+@GetMapping("/messages")
 public String getMessage() {
 	
 	return "SpringBoot project with Docker test ...";
